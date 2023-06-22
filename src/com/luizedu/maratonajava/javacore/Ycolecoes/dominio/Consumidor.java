19cfa1492c99d9f8a179bd14ffce199676546959
Luiz_E <luizedumartins21@gmail.com>
2023-06-22 20:50:39 -0300

    179 - Map, HashMap, pt 2

---
+++ src/com/luizedu/maratonajava/javacore/Ycolecoes/dominio/Consumidor.java
@@ -0,0 +1,39 @@
+package com.luizedu.maratonajava.javacore.Ycolecoes.dominio;
+
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Consumidor {
+    private long id;
+    private String nome;
+
+    public Consumidor(String nome) {
+        this.id = ThreadLocalRandom.current().nextLong(0, 100_000);
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Consumidor{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Consumidor that = (Consumidor) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
