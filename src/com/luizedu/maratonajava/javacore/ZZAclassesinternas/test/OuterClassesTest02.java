853e6f6e32f6f24ee920df6d81f390738b962af3
Luiz_E <luizedumartins21@gmail.com>
2023-07-02 18:12:37 -0300

    190 - Classes internas pt 2 - classes locais

---
+++ src/com/luizedu/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest02.java
@@ -0,0 +1,21 @@
+package com.luizedu.maratonajava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassesTest02 {
+
+    private String name = "Midoriya";
+
+    void print() {
+        String lastName = "Izuku";
+        final class LocalClass {
+            public void printLocal() {
+                System.out.println(name + " " + lastName);
+            }
+        }
+        new LocalClass().printLocal();
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest02 outer = new OuterClassesTest02();
+        outer.print();
+    }
+}
