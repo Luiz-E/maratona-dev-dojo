f8a025413197f7b10fe3b674986cffb002fb0fa9
Luiz_E <luizedumartins21@gmail.com>
2023-07-27 22:54:30 -0300

    224 - Thread p5 - sincronismo de thread pt 2

--- src/com/luizedu/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
+++ src/com/luizedu/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -5,6 +5,7 @@ import com.luizedu.maratonajava.javacore.ZZFthreads.dominio.Account;
 public class ThreadAccountTest01 implements Runnable {
 
     private Account account = new Account();
+    private final Account account2 = new Account();
 
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
@@ -25,13 +26,35 @@ public class ThreadAccountTest01 implements Runnable {
         }
     }
 
-    private void withdrawal(int amount) {
+    private synchronized void withdrawal(int amount) {
         if (account.getBalance() >= amount) {
             System.out.println(getThreadName() + " está indo sacar dinheiro");
             account.withdrawal(amount);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println(getThreadName() + " completou o saque. Valor atual da conta: " + account.getBalance());
         } else {
             System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque: " + account.getBalance());
+        }
+    }
+
+    private void withdrawal2(int amount) {
+        synchronized (account2) {
+            if (account2.getBalance() >= amount) {
+                System.out.println(getThreadName() + " está indo sacar dinheiro");
+                account2.withdrawal(amount);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(getThreadName() + " completou o saque. Valor atual da conta: " + account2.getBalance());
+            } else {
+                System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque: " + account2.getBalance());
+            }
         }
     }
 
