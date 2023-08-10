34430516344c477975e8d26e146607e31f4fc313
Luiz_E <luizedumartins21@gmail.com>
2023-08-10 18:56:22 -0300

    230, 231 - Concorrência - ReentrantLock, Conditions

---
+++ src/com/luizedu/maratonajava/javacore/ZZGConcorrencia/dominio/Members.java
@@ -0,0 +1,66 @@
+package com.luizedu.maratonajava.javacore.ZZGConcorrencia.dominio;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Members {
+    private final Queue<String> emails = new ArrayBlockingQueue<>(10);
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+    private boolean open = true;
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public int pendingEmails() {
+        lock.lock();
+        try {
+            return emails.size();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void addMemberEmail(String email) {
+        lock.lock();
+        try {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " adicionou email na lista");
+            this.emails.add(email);
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public String retrieveEmail() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " checking if there are emails");
+        lock.lock();
+        try {
+            while (emails.isEmpty()) {
+                if (!open) return null;
+                System.out.println(Thread.currentThread().getName() + " não tem email disponível na lista\nEntrando em modo de espera");
+                condition.await();
+            }
+            return emails.poll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void close() {
+        open = false;
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + " notificando todo mundo que não estamos mais recebendo emails");
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
